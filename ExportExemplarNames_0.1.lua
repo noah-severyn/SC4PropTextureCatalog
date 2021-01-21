b66cceb0590ba0e442fcdf37498b379d1e574ddf
noah-severyn <noseveryn17@gmail.com>
2021-01-20 22:11:34 -0500

    Update ExportExemplarNames_0.1.lua

--- ExportExemplarNames_0.1.lua
+++ ExportExemplarNames_0.1.lua
@@ -1,26 +1,27 @@
 -- ===================================================================================================================
--- Name: 		reader.main
--- Desc: 		writes Type, Group, Instance, Exemplar name as comma separated list to text file
+-- Name: 		main
+-- Desc: 		Writes Type, Group, Instance, Exemplar name as comma separated list to text file
 -- Params: 		N/A
 -- Called by: 	N/A
 -- Comments: 	N/A
 -- ===================================================================================================================
 function reader.main (this)   
     local file = io.open("C:/Users/Administrator/OneDrive/SC4 Deps/SC4PropTextureCatalog/working/exemplarexport.txt", "w")
-    entryidx = reader:entries_GetHeadPosition()     
+	packName = "_placeholder_"
+    entryidx = reader:entries_GetHeadPosition()
     while entryidx~=0 do
         entry,entryidx = reader:entries_GetNext(entryidx)
-        flag = reader:entry_GetFlag(entry)  
+        flag = reader:entry_GetFlag(entry)
         if (flag == 7) then
-            exemplar = reader:ex_Decode(entry) 
-            exemplaridx = reader:exprop_GetHeadPosition(exemplar)  
+            exemplar = reader:ex_Decode(entry)
+            exemplaridx = reader:exprop_GetHeadPosition(exemplar)
             while exemplaridx~=0 do
                 exprop, exemplaridx = reader:exprop_GetNext(exemplar,exemplaridx)
-                desc, descname = reader:exprop_GetDesc(exprop)  
+                desc, descname = reader:exprop_GetDesc(exprop)
                 valuestr = reader:exprop_GetValueStr(exprop)
 				T,G,I = reader:entry_GetTGI(entry)
                 if (descname == "Exemplar Name") then
-                    file:write(Dec2Hex(T,8),",",Dec2Hex(G,8),",",Dec2Hex(I,8),",",valuestr,"\n")
+                    file:write(packName,",",Dec2Hex(T,8)," ",Dec2Hex(G,8)," ",Dec2Hex(I,8)," ",valuestr,"\n")
 					break
                 end
             end
@@ -31,13 +32,13 @@ function reader.main (this)
 end
 
 
---====================================================================================================================
--- Name:		Dec2Hex
+-- ===================================================================================================================
+-- Name: 		Dec2Hex
 -- Desc: 		Returns a hex number prepended (if necessary) with zeros up to a specified length
 -- Params: 		dec ... decimal number to convert
 -- 				hexLen ... length to prepend zeros if hex is shorter than specified length (usually 8)
 -- Called by: 	reader.main
--- Comments: 	N/A
+-- Comments: 	(1) Original code credit to CorinaMarie: https://community.simtropolis.com/forums/topic/759549-coris-lua-coding-explorations-for-reader-154/
 -- ===================================================================================================================
 function Dec2Hex(dec,hexLen)
     hex = string.rep("0", (hexLen - (string.len(string.format("%x", dec))))) .. string.format("%x", dec)
