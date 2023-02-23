040466511ff313dbaae0afc56124cddc93da2db1
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2023-02-22 23:25:41 -0500

    add page for viewing a pack

---
+++ Pages/ViewPack.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SC4PropTextureCatalog.Pages
+{
+    public class ViewPackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
