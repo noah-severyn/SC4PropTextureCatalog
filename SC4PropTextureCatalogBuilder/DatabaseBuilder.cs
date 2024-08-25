4e05d1cc353a02903e9e5bf295b2932ec35c8b2d
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2024-08-25 13:46:23 -0400

    update to use folders for packs instead of files

--- SC4PropTextureCatalogBuilder/DatabaseBuilder.cs
+++ SC4PropTextureCatalogBuilder/DatabaseBuilder.cs
@@ -16,8 +16,8 @@ namespace SC4PropTextureCatalog {
         [Column("TGIID")]
         public int ID { get; set; }
 
-        [Column("PackID")]
-        public int PackID { get; set; }
+        [Column("PkgId")]
+        public int PkgId { get; set; }
 
         [Column("TGI")]
         public string TGI { get; set; }
@@ -32,24 +32,24 @@ namespace SC4PropTextureCatalog {
         //public byte[] Thumbnail { get; set; }
 
         public override string ToString() {
-            return $"{ID}: {PackID}, {TGI}, {TGIType}, {ExemplarName}";
+            return $"{ID}: {PkgId}, {TGI}, {TGIType}, {ExemplarName}";
         }
     }
 
     /// <summary>
-    /// An item in the PackTable, which tracks information about each dependency pack (aka download or file or path).
+    /// An item in the PackTable, which tracks information about each dependency package (aka download or file or path).
     /// </summary>
-    [Table("PackTable")]
-    public class PackItem {
+    [Table("PackageTable")]
+    public class PackageItem {
         [PrimaryKey, AutoIncrement]
-        [Column("PackID")]
-        public int PackID { get; set; }
+        [Column("PkgId")]
+        public int PkgId { get; set; }
 
-        [Column("PackName")]
-        public string PackName { get; set; }
+        [Column("PkgName")]
+        public string PkgName { get; set; }
 
         [Column("PackVersion")]
-        public string PackVersion { get; set; }
+        public string PkgVersion { get; set; }
 
         [Column("Hyperlink")]
         public string Hyperlink { get; set; }
@@ -67,12 +67,12 @@ namespace SC4PropTextureCatalog {
         public string SecondaryCat { get; set; }
 
         public override string ToString() {
-            return $"{PackID}: {PackName}, (v{PackVersion}) by {Author}. Type:{Type}, Primary:{PrimaryCat}, Secondary:{SecondaryCat}";
+            return $"{PkgId}: {PkgName}, (v{PkgVersion}) by {Author}. Type:{Type}, Primary:{PrimaryCat}, Secondary:{SecondaryCat}";
         }
     }
 
     /// <summary>
-    /// Dimension 
+    /// Dimension/lookup table of TGI types (building, prop, texture, flora, cohort).
     /// </summary>
     [Table("TGITypes")]
     public class TGICategory {
@@ -94,61 +94,153 @@ namespace SC4PropTextureCatalog {
     /// Create and operate on the Prop Texture Catalog database.
     /// </summary>
     public class DatabaseBuilder {
-        private readonly SQLiteConnection _db;
-        private readonly List<FileInfo> _files;
+        private SQLiteConnection _db;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="dbpath">File path to save the database as</param>
-        /// <param name="dbpfFiles">List of file paths to iterate through</param>
-        public DatabaseBuilder(string dbpath, List<FileInfo> dbpfFiles) {
-            _files = dbpfFiles;
-            dbpath = Path.Combine(dbpath, "Catalog.db");
-            if (File.Exists(dbpath)) {
-                File.Delete(dbpath);
+        public DatabaseBuilder() {
+            string dbPath = "C:\\source\\repos\\SC4PropTextureCatalog\\SC4PropTextureCatalogBuilder\\data\\Catalog.db";
+            if (File.Exists(dbPath)) {
+                File.Delete(dbPath);
             }
 
             //Open a new StreamWriter to create a file then immediately close - writing is handled by the SQLite functions
-            File.CreateText(dbpath).Dispose();
+            File.CreateText(dbPath).Dispose();
+            _db = new SQLiteConnection(dbPath);
+        }
 
+        /// <summary>
+        /// Build a SQLite DB of props and lot textures from the list of files.
+        /// </summary>
+        public void BuildDB() {
             //Initialize database tables and schema
-            _db = new SQLiteConnection(dbpath);
             _db.CreateTable<TGIItem>();
-            _db.CreateTable<PackItem>();
+            _db.CreateTable<PackageItem>();
             _db.CreateTable<TGICategory>();
             _db.Insert(new TGICategory { TGIType = 0, TGIName = "Building" });
             _db.Insert(new TGICategory { TGIType = 1, TGIName = "Prop" });
             _db.Insert(new TGICategory { TGIType = 2, TGIName = "Texture" });
             _db.Insert(new TGICategory { TGIType = 4, TGIName = "Flora" });
             _db.Insert(new TGICategory { TGIType = 10, TGIName = "Cohort" });
+
+            string folderToScan = "C:\\Users\\Administrator\\OneDrive\\SC4 Deps\\new";
+            List<string> folders = Directory.EnumerateDirectories(folderToScan).ToList();
+            StringBuilder log = new StringBuilder("FileName,Type,Group,Instance,TGIType,TGISubtype,Message");
+            int pkgIdx = 0;
+
+            foreach (string pkg in folders) {
+                List<string> files = Directory.EnumerateFiles(pkg).ToList();
+                foreach (string file in files) {
+                    Debug.WriteLine(file);
+                    if (!DBPFUtil.IsValidDBPF(file)) {
+                        continue;
+                    }
+
+                    DBPFFile dbpf = new DBPFFile(file);
+                    log.AppendLine(dbpf.GetIssueLog());
+                    string filename = Path.GetFileNameWithoutExtension(file);
+                    _db.Insert(new PackageItem {
+                        PkgId = pkgIdx,
+                        PkgName = filename
+                    });
+
+
+                    foreach (DBPFEntry entry in dbpf.GetEntries()) {
+                        //Add Base/Overlay textures (look at the least significant 4 bits and only add if it is 0, 5, or A: AND the Instance by 0b1111 (0xF) and examine the modulus result)
+                        if (entry.MatchesEntryType(DBPFTGI.FSH_BASE_OVERLAY) && ((entry.TGI.InstanceID & 0xF) % 5) == 0) {
+                            AddTGI(filename, entry.TGI.ToString().Substring(0, 34), 2, null);
+                        }
+
+
+                        //Add Exemplars
+                        else if (entry.MatchesEntryType(DBPFTGI.EXEMPLAR)) {
+                            DBPFEntryEXMP exmp = (DBPFEntryEXMP) entry;
+                            entry.Decode();
+                            if (exmp.ListOfProperties.Count == 0) continue;
+
+                            DBPFProperty.ExemplarType exmpType = exmp.GetExemplarType();
+                            if (exmpType == DBPFProperty.ExemplarType.UnknownType) {
+                                log.AppendLine($"{filename}, {exmp.TGI.ToString().Replace(" ", "")},missing property: ExemplarType");
+                                exmpType = 0;
+                            }
+
+                            DBPFProperty prop = exmp.GetProperty("ExemplarName");
+                            string exmpName;
+                            if (prop is null) {
+                                log.AppendLine($"{filename}, {exmp.TGI.ToString().Replace(" ", "")},missing property: ExemplarName");
+                                exmpName = "";
+                            } else {
+                                exmpName = exmpName = (string) prop.GetData();
+                            }
+
+                            switch (exmpType) {
+                                case DBPFProperty.ExemplarType.UnknownType:
+                                    AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), null, exmpName);
+                                    break;
+                                case DBPFProperty.ExemplarType.Building:
+                                    AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), 0, exmpName);
+                                    break;
+                                case DBPFProperty.ExemplarType.Prop:
+                                    AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), 1, exmpName);
+                                    break;
+                                case DBPFProperty.ExemplarType.FloraFauna:
+                                    AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), 4, exmpName);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+
+                        //Add Cohorts (note the building/prop family of the cohort is always 0x10000000 less than the Cohort's Index)
+                        else if (entry.MatchesEntryType(DBPFTGI.COHORT)) {
+                            DBPFEntryEXMP exmp = (DBPFEntryEXMP) entry;
+                            TGI family = new TGI(entry.TGI.TypeID, entry.TGI.GroupID, entry.TGI.InstanceID - 0x10000000);
+                            exmp.Decode();
+                            if (exmp.ListOfProperties.Count == 0) continue;
+                            string exmpName;
+                            try {
+                                exmpName = (string) exmp.GetProperty("ExemplarName").GetData();
+                            }
+                            catch (Exception) {
+                                exmpName = "??";
+                            }
+                            
+                            AddTGI(filename, family.ToString().Substring(0, 34), 10, exmpName);
+                        }
+                    }
+                }
+
+                pkgIdx++;
+            }
+
+            File.WriteAllText("C:\\source\\repos\\SC4PropTextureCatalog\\SC4PropTextureCatalogBuilder\\data\\log.csv", log.ToString());
         }
 
 
 
+
         /// <summary>
-        /// Add a TGI item with associated information to the database.
+        /// Add a TGI item to the database.
         /// </summary>
-        /// <param name="packName">Name of dependency pack the TGI is contained in</param>
+        /// <param name="pkgName">Name of dependency package the TGI is contained in</param>
         /// <param name="tgi">String representation of the TGI in the format 0x00000000, 0x00000000, 0x00000000 </param>
         /// <param name="tgitype">Type of TGI: Building=0, Prop=1, Texture=2, Flora=4, Cohort=10</param>
         /// <param name="exmpName">Name of the exemplar; null if the TGI is a texture</param>
         /// <remarks>The path in TGITable is stored as a reference to the full path in PathTable. This dramatically reduces file size as the long path string only needs to be stored once.</remarks>
-        private void AddTGI(string packName, string tgi, int? tgitype, string exmpName) {
-            //check if we already have a matching PackID, create new PackItem if not, otherwise use found PackID
-            int packID = GetPackID(packName);
+        private void AddTGI(string pkgName, string tgi, int? tgitype, string exmpName) {
+            //check if we already have a matching PkgId, create new PackItem if not, otherwise use found PkgId
+            int packID = GetPkgId(pkgName);
             if (packID <= 0) {
 
-                PackItem newPack = new PackItem {
-                    PackName = packName
+                PackageItem newPack = new PackageItem {
+                    PkgName = pkgName
                 };
                 _db.Insert(newPack);
-                packID = newPack.PackID;
+                packID = newPack.PkgId;
             }
 
-            //once we know our PackID then add the new TGI with that PackID
+            //once we know our PkgId then add the new TGI with that PkgId
             TGIItem newTGI = new TGIItem {
-                PackID = packID,
+                PkgId = packID,
                 TGI = tgi,
                 TGIType = tgitype,
                 ExemplarName = exmpName
@@ -162,15 +254,15 @@ namespace SC4PropTextureCatalog {
         /// <summary>
         /// Lookup and return the PathID for the provided PathName.
         /// </summary>
-        /// <param name="packName">Pack name (file name) to lookup</param>
+        /// <param name="pkgName">Pack name (file name) to lookup</param>
         /// <returns>PathID if name is found; -1 if item is not found or if multiple matches were found</returns>
         /// <remarks>PathItems table should always have unique items, so we might have an issue if the return is more than one item.</remarks>
-        private int GetPackID(string packName) {
-            string searchName = packName.Replace("'", "''");
-            List<PackItem> result = _db.Query<PackItem>($"SELECT * FROM PackTable WHERE PackName = '{searchName}'");
+        private int GetPkgId(string pkgName) {
+            string searchName = pkgName.Replace("'", "''");
+            List<PackageItem> result = _db.Query<PackageItem>($"SELECT * FROM PackageTable WHERE PkgName = '{searchName}'");
 
             if (result.Count == 1) {
-                return result[0].PackID;
+                return result[0].PkgId;
             } else {
                 return -1;
             }
@@ -190,7 +282,7 @@ namespace SC4PropTextureCatalog {
         /// </summary>
         /// <param name="tgi">TGI to use</param>
         /// <returns>A PNG image represented as bytes</returns>
-        private byte[] GetThumbnail(string tgi) {
+        private static byte[] GetThumbnail(string tgi) {
             string fname = tgi.Replace("0x", "").Replace(", ", "-") + ".png";
             try {
                 return File.ReadAllBytes("C:\\source\\repos\\SC4PropTextureCatalog\\wwwroot\\img\\thumbnails\\" + fname);
@@ -198,92 +290,6 @@ namespace SC4PropTextureCatalog {
                 return new byte[0];
             }
             
-        }
-
-
-
-        /// <summary>
-        /// Creates a SQLite DB of props and lot textures from the list of files.
-        /// </summary>
-        public void BuildDB() {
-            StringBuilder log = new StringBuilder("FileName,Type,Group,Instance,TGIType,TGISubtype,Message");
-
-            for (int idx = 0; idx < _files.Count; idx++) {
-                FileInfo file = _files[idx];
-
-                //Debug.WriteLine(file.Name);
-                DBPFFile dbpf = new DBPFFile(file);
-                log.AppendLine(dbpf.GetIssueLog());
-
-                _db.Insert(new PackItem { 
-                    PackID = idx, 
-                    PackName = file.Name
-                });
-                
-                foreach (DBPFEntry entry in dbpf.GetEntries()) {
-                    string filename = Path.GetFileNameWithoutExtension(file.Name);
-
-
-                    //Add Base/Overlay textures (look at the least significant 4 bits and only add if it is 0, 5, or A: AND the Instance by 0b1111 (0xF) and examine the modulus result)
-                    if (entry.MatchesEntryType(DBPFTGI.FSH_BASE_OVERLAY) && ((entry.TGI.InstanceID & 0xF) % 5) == 0) {
-                        AddTGI(filename, entry.TGI.ToString().Substring(0, 34), 2, null);
-                    }
-
-
-                    //Add Exemplars
-                    else if (entry.MatchesEntryType(DBPFTGI.EXEMPLAR)) {
-                        DBPFEntryEXMP exmp = (DBPFEntryEXMP) entry;
-                        entry.Decode();
-                        if (exmp.ListOfProperties.Count == 0) continue;
-
-                        DBPFProperty.ExemplarType exmpType = exmp.GetExemplarType();
-                        if (exmpType == DBPFProperty.ExemplarType.UnknownType) {
-                            log.AppendLine($"{filename}, {exmp.TGI.ToString().Replace(" ", "")},missing property: ExemplarType");
-                            exmpType = 0;
-                        }
-
-                        DBPFProperty prop = exmp.GetProperty("ExemplarName");
-                        string exmpName;
-                        if (prop is null) {
-                            log.AppendLine($"{filename}, {exmp.TGI.ToString().Replace(" ", "")},missing property: ExemplarName");
-                            exmpName = "";
-                        } else {
-                            exmpName = exmpName = (string) prop.GetData();
-                        }
-
-                        switch (exmpType) {
-                            case DBPFProperty.ExemplarType.UnknownType:
-                                AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), null, exmpName);
-                                break;
-                            case DBPFProperty.ExemplarType.Building:
-                                AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), 0, exmpName);
-                                break;
-                            case DBPFProperty.ExemplarType.Prop:
-                                AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), 1, exmpName);
-                                break;
-                            case DBPFProperty.ExemplarType.FloraFauna:
-                                AddTGI(filename, exmp.TGI.ToString().Substring(0, 34), 4, exmpName);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-
-
-                    //Add Cohorts (note the building/prop family of the cohort is always 0x10000000 less than the Cohort's Index)
-                    else if (entry.MatchesEntryType(DBPFTGI.COHORT)) {
-                        DBPFEntryEXMP exmp = (DBPFEntryEXMP) entry;
-                        TGI family = new TGI(entry.TGI.TypeID, entry.TGI.GroupID, entry.TGI.InstanceID - 0x10000000);
-                        entry.Decode();
-                        if (exmp.ListOfProperties.Count == 0) continue;
-
-                        string exmpName = (string) exmp.GetProperty("ExemplarName").GetData();
-                        AddTGI(filename, family.ToString().Substring(0, 34), 10, exmpName);
-                    }
-                }
-            }
-
-            File.WriteAllText("C:\\source\\repos\\SC4PropTextureCatalog\\Data\\log.csv", log.ToString());
         }
     }
 }
--- SC4PropTextureCatalogBuilder/Program.cs
+++ SC4PropTextureCatalogBuilder/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using csDBPF;
+using SC4PropTextureCatalog;
+
+DatabaseBuilder _db = new DatabaseBuilder();
+_db.BuildDB();
