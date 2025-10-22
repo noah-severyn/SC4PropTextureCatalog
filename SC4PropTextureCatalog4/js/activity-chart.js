671c813bf521179926f70794c61f468e861694a9
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2025-10-21 20:38:37 -0400

    added non sc4 stex ids to exclude

--- SC4PropTextureCatalog4/js/activity-chart.js
+++ SC4PropTextureCatalog4/js/activity-chart.js
@@ -2,23 +2,59 @@ const STEX_MAX_ID = 37061;
 const CHUNK_SIZE = 250;
 const CHUNK_COUNT = Math.ceil(STEX_MAX_ID / CHUNK_SIZE);
 
-let stex_data = [];
-fetch('data/stex-data.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    stex_data = data;
-    const progress = document.getElementById('stex-progress');
-    progress.setAttribute('value', data.length);
-	  progress.setAttribute('max', STEX_MAX_ID);
-    document.getElementById('stex-progress-label').innerText = data.length + "/" + STEX_MAX_ID + ' (' + (data.length / STEX_MAX_ID * 100).toLocaleString(undefined, {maximumFractionDigits: 2}) + '%) of uploads are indexed'
-  });
+// Create list of STEX ids for non-sc4pac files
+let non_sc4pac_data = [];
+const fileIds = ["003", "010", "012", "013", "015", "016", "017", "019", "020", "021", "022", "023", "024", "026", "027", "029", "030", "031", "038", "041", "042", "043", 115, 116, 117, 119, 121, 150, 156];
+const non_sc4pac_files = fileIds.map(id => `data/exclusions/stex-cat-${id}.json`);
+Promise.all(
+	non_sc4pac_files.map(file =>
+		fetch(file)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+				return response.json();
+			})
+	)
+).then(results => {
+	results.forEach(data => {
+		non_sc4pac_data = non_sc4pac_data.concat(data);
+	});
+});
 
-// stex_data = stex_data.slice(0, 3000);
+
+let all_data = [];
+fetch('data/stex-data.json')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		return response.json();
+	})
+	.then(data => {
+		all_data = all_data.concat(data);
+		const progress = document.getElementById('stex-progress');
+		const sc4FilesCount = STEX_MAX_ID - non_sc4pac_files.length
+		
+		progress.setAttribute('value', all_data.length);
+		progress.setAttribute('max', sc4FilesCount);
+		document.getElementById('stex-progress-label').innerText = all_data.length + "/" + sc4FilesCount + ' (' + (all_data.length / sc4FilesCount * 100).toLocaleString(undefined, { maximumFractionDigits: 2 }) + '%) of uploads are indexed'
+	});
+
+fetch('data/default-data.json')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		return response.json();
+	})
+	.then(data => {
+		all_data = all_data.concat(data);
+		const progress = document.getElementById('stex-progress');
+		progress.setAttribute('value', all_data.length);
+		progress.setAttribute('max', STEX_MAX_ID);
+		document.getElementById('stex-progress-label').innerText = all_data.length + "/" + (STEX_MAX_ID - non_sc4pac_files.length) + ' (' + (all_data.length / (STEX_MAX_ID - non_sc4pac_files.length) * 100).toLocaleString(undefined, { maximumFractionDigits: 2 }) + '%) of uploads are indexed'
+	});
 
 
 
@@ -26,12 +62,12 @@ const chart = document.getElementById('activity-chart');
 
 // Create placeholders for N chunks
 for (let i = 0; i < CHUNK_COUNT; i++) {
-    const placeholder = document.createElement('div');
-    placeholder.className = 'chunk-placeholder';
-    placeholder.setAttribute('data-tooltip', i);
+	const placeholder = document.createElement('div');
+	placeholder.className = 'chunk-placeholder';
+	placeholder.setAttribute('data-tooltip', i);
 	placeholder.dataset.index = i;
-    placeholder.style.minHeight = '15px';  // visual space so user can scroll
-    //placeholder.textContent = i;
+	placeholder.style.minHeight = '15px';  // visual space so user can scroll
+	//placeholder.textContent = i;
 	chart.appendChild(placeholder);
 }
 
@@ -43,8 +79,12 @@ function loadChunk(index, placeholder) {
 	for (var idx = 1; idx <= CHUNK_SIZE; idx++) {
 		const square = document.createElement('button');
 		const itemId = CHUNK_SIZE * index + idx;
-		const item = stex_data.find((i) => i.id === itemId);
-		if (item !== undefined) {
+		const item = all_data.find((i) => i.id === itemId);
+		if (non_sc4pac_data.some(i => i.id === itemId)) {
+			square.style.backgroundColor = '#fafafa';
+			square.classList.add("nohover");
+		}
+		else if	(item !== undefined) {
 			square.style.backgroundColor = '#507a99';
 			square.setAttribute('data-tooltip', item.name);
 			square.setAttribute('onclick', "window.open('" + item.url + "', '_blank')");
@@ -52,12 +92,10 @@ function loadChunk(index, placeholder) {
 		else {
 			square.setAttribute('data-tooltip', itemId);
 		}
-		square.className = 'square';
+		square.classList.add("square");
 		square.title = itemId;
-
 		frag.appendChild(square);
 	}
-	
 
 	// Replace placeholder with actual content
 	placeholder.replaceWith(frag);
