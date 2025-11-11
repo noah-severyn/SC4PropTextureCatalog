0003280bc85913c07014c3558b554706a3ac1e4e
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2025-11-10 22:26:25 -0500

    update app router

--- SC4PropTextureCatalogAPI/app.js
+++ SC4PropTextureCatalogAPI/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const apiRoutes = require('./routes/api');
+import express from 'express';
+import apiRoutes from './routes/api.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
