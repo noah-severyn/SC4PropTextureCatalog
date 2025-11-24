5463e7eb0a265d5b5e5c2e063299714267690e89
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2025-11-23 20:19:48 -0500

    enable cors

--- SC4PropTextureCatalogAPI/app.js
+++ SC4PropTextureCatalogAPI/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import apiRoutes from './routes/api.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({Origin: 'http://127.0.0.1:3000' }));
 app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
