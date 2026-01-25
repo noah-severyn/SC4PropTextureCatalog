0616a62757156d12d14f2d92bd45774e5d0ab664
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2026-01-25 10:54:35 -0500

    extract site header to centralized function
    updates to the header now only need to be made in one place, instead of every page

--- SC4PropTextureCatalog4/js/site.js
+++ SC4PropTextureCatalog4/js/site.js
@@ -6,6 +6,44 @@ if (prod) {
 } else {
 	apiUrl = 'http://localhost:4000';
 }
+AddHeader();
 
+/**
+ * Add the site header and navigation
+ */
+function AddHeader() {
+	const header = document.createElement('header');
+	const nav = document.createElement('nav');
+	nav.className = "container";
+	const ul = document.createElement('ul');
+	
+	const title = document.createElement('li');
+	title.className = "logo";
+	title.textContent = "SC4PropTextureCatalog";
+	ul.appendChild(title);
+	
+	const pages = [
+		{ name: 'Home', link: '/SC4PropTextureCatalog4/Index.html' },
+		{ name: 'View a Pack', link: '/SC4PropTextureCatalog4/ViewPack.html' },
+		{ name: 'About', link: '/SC4PropTextureCatalog4/About.html' }
+	];
+	pages.forEach(page => {
+		const li = document.createElement('li');
+		const a = document.createElement('a');
+		a.href = page.link;
+		a.textContent = page.name;
+		li.appendChild(a);
+		ul.appendChild(li);
+	});
 
+	const theme = document.createElement('li');
+	const themeLink = document.createElement('a');
+	themeLink.href = "#";
+	themeLink.id = "theme_switcher";
+	theme.appendChild(themeLink);
+	ul.appendChild(theme);
 
+	nav.appendChild(ul);
+	header.appendChild(nav);
+	document.body.insertBefore(header, document.body.firstChild);
+}
